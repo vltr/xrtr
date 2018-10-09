fdf8664182887cd584bff1f43cd8777291552e2e
Richard Kuesters <rkuesters@gmail.com>
2018-10-09 16:23:25 -0300

    Bump version: 0.2.0 → 0.2.1

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ project = 'xrtr'
 year = '2018'
 author = 'Richard Kuesters'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '0.2.0'
+version = release = '0.2.1'
 
 pygments_style = 'default'
 templates_path = ['.']
